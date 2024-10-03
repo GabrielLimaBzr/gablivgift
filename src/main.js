0982af88c10f92bed0e0cbc2b884ca415148d528
gabriel.bezerra <gabriel18lb@outlook.com>
2024-10-03 20:22:04 -0300

    primeiro commit

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createVuestic } from 'vuestic-ui'
+import 'vuestic-ui/styles/essential.css'
+import 'vuestic-ui/styles/typography.css'
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+
+
+app.use(createVuestic());
+app.mount('#app')
