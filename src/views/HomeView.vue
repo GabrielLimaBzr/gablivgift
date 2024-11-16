8b35b3d75f85146aad7667f044c9b8f62a41baba
gabriel.bezerra <gabriel18lb@outlook.com>
2024-11-16 18:50:05 -0300

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
     <div class="flex flex-col justify-center w-full items-center py-10">
         <h1 class="title">GabLivGifts</h1>
         <p class="sub">Aqui dividimos nossa lista de <span class="fra">presentes</span>, contem uma imensa lista de desesjos!</p>
-        <div class="w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-10 p-2">
+        <div class="w-full grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-7 mt-10 p-2">
             <Card></Card>
             <Card></Card>
             <Card></Card>
