b7180991e14ffb3ef67926c3772dc3a5853fd38a
gabriel.bezerra <gabriel18lb@outlook.com>
2024-11-22 14:01:51 -0300

    vercel speed insights

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
+import { SpeedInsights } from '@vercel/speed-insights/vue';
+
 </script>
 
 <template>
+  <SpeedInsights />
   <RouterView></RouterView>
 </template>
 
 <style>
-
 * {
   box-sizing: border-box;
 }
-
-
 </style>
