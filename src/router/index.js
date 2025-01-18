6eeb6a2a4df2addcb30b5578b1f1e955ca3741d7
gabriel.bezerra <gabriel18lb@outlook.com>
2025-01-18 19:20:03 -0300

    fix

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import { jwtDecode } from "jwt-decode";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
