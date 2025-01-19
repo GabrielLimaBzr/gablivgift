a33e9cc372dcf5f6a504d65d64438b846aa9bc63
GabrielLimaBzr <gabriel18lb@outlook.com>
2025-01-19 20:42:07 -0300

    fix url backend

--- src/services/giftService.js
+++ src/services/giftService.js
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
-const END_POINT = import.meta.env.WS_URL_API + '/gift';
+const apiBaseUrl = import.meta.env.VITE_BACKEND_URL + '/gift';
 
 export async function getAllGifts() {
   try {
     console.log("END_POINT: ", END_POINT);
     
-    const response = await axios.get(END_POINT+'/gifts', {
+    const response = await axios.get(apiBaseUrl+'/gifts', {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${localStorage.getItem('authToken')}`
