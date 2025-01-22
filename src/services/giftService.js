ef7a17acdd57c046cfdd44daae7ba7c1f6ce97c2
GabrielLimaBzr <gabriel18lb@outlook.com>
2025-01-22 20:58:35 -0300

    fix gift service

--- src/services/giftService.js
+++ src/services/giftService.js
@@ -4,17 +4,51 @@ const apiBaseUrl = import.meta.env.VITE_BACKEND_URL + '/gift';
 
 export async function getAllGifts() {
   try {
-    const endpoint = apiBaseUrl+'/gifts';
-    console.log("END_POINT: ",Date.now() , endpoint);
+    const endpoint = apiBaseUrl + '/gifts';
+    console.log("END_POINT: ", Date.now(), endpoint);
     const response = await axios.get(endpoint, {
       headers: {
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${localStorage.getItem('authToken')}`
       }
     });
-    
-    console.log("response: ",Date.now(), response.data);
+
+    console.log("response: ", Date.now(), response.data);
     return response.data.gifts;
+  } catch (error) {
+    console.error("Erro chamada api: ", error);
+    return null
+  }
+}
+
+export async function uploadImageGift(formData) {
+  try {
+    const endpoint = apiBaseUrl + '/upload';
+    const response = await axios.post(endpoint, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+        'Authorization': `Bearer ${localStorage.getItem('authToken')}`
+      }
+    });
+
+    return response.data.url;
+
+  } catch (error) {
+    console.error("Erro chamada api: ", error);
+    return null
+  }
+}
+
+export async function createGift(gift) {
+  try {
+    const endpoint = apiBaseUrl + '/create-gift';
+    const response = await axios.post(endpoint, gift, {
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${localStorage.getItem('authToken')}`
+      }
+    });
+    return response;
   } catch (error) {
     console.error("Erro chamada api: ", error);
     return null
