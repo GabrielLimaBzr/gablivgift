69475661610d8bead22db58e50ddd648d8967fef
GabrielLimaBzr <gabriel18lb@outlook.com>
2025-01-25 14:37:51 -0300

    adiciona novos componentes Vuestic ao aplicativo

--- src/main.js
+++ src/main.js
@@ -21,7 +21,34 @@ app.use(createVuestic({
                 backgroundSecondary: "#292929",
                 customBg: "#2929296a"
             }
-        }
+        },
+        components: {
+            VaButton: true,
+            VaInput: true,
+            VaChip: true,
+            VaScrollContainer: true,
+            VaSelect: true,
+            VaModal: true,
+            VaIcon: true,
+            VaForm: true,
+            VaInputWrapper: true,
+            VaTextarea: true,
+            VaFileUpload: true,
+            VaFormField: true,
+            VaSwitch: true,
+            VaButtonDropdown: true,
+            VaMenu: true,
+            VaMenuList: true,
+            VaMenuItem: true,
+            VaDivider: true,
+            VaCard: true,
+            VaCardContent: true,
+            VaSkeleton: true,
+            VaCardActions: true,
+            VaPagination: true,
+            VaSkeletonGroup: true,
+            
+        },
     }
 }));
 app.mount('#app')
